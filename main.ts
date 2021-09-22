d835fd6d93d8cc00b81dda8fa96e59c9c5308b0c
Benny van den Hoogen <benny@vriendvandeshow.nl>
2021-09-22 17:39:10 +0200

    Removed unused import statements

--- main.ts
+++ main.ts
@@ -1,7 +1,3 @@
-import { Client } from "https://deno.land/x/mysql/mod.ts";
-import * as config from "./config.ts";
-import * as emoji from "https://deno.land/x/emoji/mod.ts";
-import { config as dotEnvConfig } from 'https://deno.land/x/dotenv@v1.0.1/mod.ts';
 import * as fetcher from './app/fetcher.ts';
 
 await fetcher.runFetchProcess();
