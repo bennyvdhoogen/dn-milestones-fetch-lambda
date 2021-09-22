3f75a08df56dc95b0521dae9fe586459de8c752e
Benny van den Hoogen <benny@vriendvandeshow.nl>
2021-09-22 18:35:37 +0200

    Implemented paged fetching to allow fetching of more than 25 episodes per show

--- app/fetcher.ts
+++ app/fetcher.ts
@@ -39,9 +39,39 @@ export async function fetchEpisode(art19endpoint: string, episode_id: string){
     return data;
   }
 
+export async function fetchEpisodeLinksPage(originUrl: string)
+{
+  const currentResponse = await fetch(originUrl, {
+    method: "GET",
+    headers: {
+      Accept:        "application/vnd.api+json",
+      Authorization: 'Token token="'+env.ART19_TOKEN+'", credential="'+env.ART19_CREDENTIAL+'"'
+    }
+  });
+
+  const data = await currentResponse.json();
+
+  //console.log(data);
+
+  return data;
+}
+
+export async function* episodeIterator(episodesObj: any)
+{
+  while (true) {
+    const { data, links } = await fetchEpisodeLinksPage(episodesObj.links.next);
+    episodesObj.data = data;
+    episodesObj.links = links;
+    for (const item of data) {
+      yield item;
+    }
+
+    if (links.next === links.last) break;
+  }
+}
+
 export async function fetchAllEpisodes(art19endpoint: string, series_id: string){
     // fetch show
-    console.log('fetching data from ART19...');
     const response = await fetch(art19endpoint + '/series/' + series_id, {
       method: "GET",
       headers: {
@@ -50,14 +80,20 @@ export async function fetchAllEpisodes(art19endpoint: string, series_id: string)
       }
     });
 
-    console.log(config);
     // return array of episodes
     const data = await response.json();
-    console.log('done fetching..');
+    const allEps = [];
 
-    console.log(data);
+    //const allEpisodePages = await episodeIterator(data.data.relationships.episodes);
 
-    return data.data.relationships.episodes.data;
+    for await (const ep of episodeIterator(data.data.relationships.episodes)) {
+      //console.log(ep);
+      allEps.push(ep);
+    }
+
+    console.log(`Fetched ${allEps.length} episodes for ${series_id}`)
+
+    return allEps;
   }
 
 export async function insertEpisodeIntoDb(show_id: number, episode: any) {
@@ -87,7 +123,6 @@ export async function updateShowListenCount(show_id: number, total_listen_count:
 
 export async function insertMeasurementIntoDb(measurement: any) {
     // select db
-    console.log('inserting measurement in db');
     await client.execute(`USE ${config.DATABASE}`);
     // insert into episodes table
     let dbInsert = await client.execute(`
@@ -95,8 +130,6 @@ export async function insertMeasurementIntoDb(measurement: any) {
       VALUES ('${measurement.show_id}','${measurement.episode_id}','${measurement.type}',${measurement.count}, NOW())
       ON DUPLICATE KEY UPDATE count = ${measurement.count};
     `);
-
-    console.log(dbInsert);
   };
 
 
@@ -116,7 +149,6 @@ export async function calculateIncrement(measurement: any) {
     const seconds_between = secondsBetweenDates(measurement_a.date, measurement_b.date);
 
     const increment_value = measurement_b.count - measurement_a.count;
-    console.log(increment_value);
 
       await client.execute(`
       INSERT INTO increments (show_id, episode_id, previous_measurement_id, current_measurement_id, seconds_between, value, date)
@@ -129,7 +161,6 @@ export async function calculateShowDailyIncrement(show_id: number) {
     const dailyIncrease = await client.query(
       `SELECT SUM(value) as sum FROM increments WHERE show_id = '${show_id}' AND DATE(date) = DATE(NOW());`
     );
-      console.log(dailyIncrease);
 
     if(!dailyIncrease[0].sum){
       return false;
@@ -192,10 +223,6 @@ export function secondsBetweenDates(date1: string, date2: string){
     const Seconds_from_T1_to_T2 = dif / 1000;
     const Seconds_Between_Dates = Math.abs(Seconds_from_T1_to_T2);
 
-    console.log(Seconds_Between_Dates);
-    console.log(t1);
-    console.log(t2);
-
     return Seconds_Between_Dates;
   }
 
@@ -205,9 +232,7 @@ export async function iterateOverEpisodes(episodes: any) {
 
     for (const episode of episodes){
       const data = await fetchEpisode(config.art19endpoint, episode.id);
-      console.log(data);
       let dbShow = await getShowFromDb(data.data.attributes.series_id);
-      console.log(data.data.attributes.series_ids);
 
       let measurement = {
         show_id: null,
@@ -220,7 +245,6 @@ export async function iterateOverEpisodes(episodes: any) {
 
       // sum listen count
       show_total_listen_count += data.data.attributes.listen_count;
-      console.log(dbShow);
       insertEpisodeIntoDb(dbShow.id, data.data).then(async function() {
         let dbEpisode = await getEpisodeFromDb(data.data.id);
         let dbShow = await getShowFromDb(data.data.attributes.series_id);
@@ -240,56 +264,18 @@ export async function iterateOverEpisodes(episodes: any) {
         return true;
       }
     }
-
-    // episodes.forEach(async function(episode: any) {
-    //   console.log(episode);
-    //   const data = await fetchEpisode(art19endpoint, episode.id);
-    //   console.log(data);
-    //   let dbShow = await getShowFromDb(data.data.attributes.series_id);
-    //   console.log(data.data.attributes.series_ids);
-
-    //   let measurement = {
-    //     show_id: null,
-    //     episode_id: null,
-    //     art19_show_id: data.data.attributes.series_id,
-    //     art19_episode_id: data.id,
-    //     type: 'listen_count',
-    //     count: data.data.attributes.listen_count,
-    //   };
-
-    //   // sum listen count
-    //   show_total_listen_count += data.data.attributes.listen_count;
-    //   console.log(dbShow);
-    //   insertEpisodeIntoDb(dbShow.id, data.data).then(async function() {
-    //     let dbEpisode = await getEpisodeFromDb(data.data.id);
-    //     let dbShow = await getShowFromDb(data.data.attributes.series_id);
-    //     measurement.episode_id = dbEpisode.id;
-    //     measurement.show_id = dbShow.id;
-    //     insertMeasurementIntoDb(measurement);
-    //     calculateIncrement(measurement);
-    //   });
-
-
-    //   episodesProcessed++;
-
-    //   if(episodes.length == episodesProcessed){
-    //     await updateShowListenCount(dbShow.id, show_total_listen_count)
-    //     await calculateMilestones();
-    //     await calculateShowDailyIncrement(dbShow.id);
-    //   }
-    // });
   }
 
 export async function runFetchProcess(){
-  console.log('start running');
+  console.log('Start fetch program..');
   let shows = await getAllShowFromDb();
 
   for (const show of shows) {
+    console.log(`Processing ${show.title} (${show.art19_id})`)
     let episodes = await fetchAllEpisodes(config.art19endpoint, show.art19_id);
     iterateOverEpisodes(episodes);
   }
 
-  console.log('completed running');
-  console.log(shows);
+  console.log('Completed fetch program..');
   return;
 }
